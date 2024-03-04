d86ecf557ab56dd4d4af9eae43aeb8d0c6ae03da
Christian Lisangola <jochri3@gmail.com>
2024-03-04 12:19:56 +0300

    Finished events & styles

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const firstNumber = document.querySelector("#first-number");
-const secondNumber = document.querySelector("#second-number");
-const resultButton = document.querySelector("#result-button");
+const technos = document.querySelectorAll("li");
 
-resultButton.addEventListener("click", function () {
-  const resultText = document.querySelector("#result");
-  resultText.innerText = Number(firstNumber.value) + Number(secondNumber.value);
+// for (let techno of technos) {
+//   // techno.style.backgroundColor = "indigo";
+//   // techno.style.color = "orangered";
+//   // techno.style.fontSize = "20px";
+//   // techno.style.border = "1px solid orangered";
+//   // techno.classList.add("tech");
+// }
+
+for (let techno of technos) {
+  techno.addEventListener("click", function () {
+    // techno.classList.add("tech");
+    // this.classList.add("tech");
+
+    // Mauvaise pratique car tout ce  qu'on c'est d'ajouter/supprimer une classe.
+    // if (this.classList.contains("tech")) {
+    //   this.classList.remove("tech");
+    // } else {
+    //   this.classList.add("tech");
+    // }
+
+    // Bonne pratiques
+    this.classList.toggle("tech");
+  });
+}
+
+document.body.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  console.log("Les clics droit son interdits");
 });
