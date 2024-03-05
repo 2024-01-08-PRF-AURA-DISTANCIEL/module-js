8be8d4711986723fb878ddd240ba9a01541f454b
Christian Lisangola <jochri3@gmail.com>
2024-03-05 11:15:39 +0300

    Todo list project

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-import users from "./users.js";
+const todos = [
+  "Text a friend I haven't talked to in a long time",
+  "Plan a vacation I've always wanted to take",
+  "Take cat on a walk",
+  "Go to the gym",
+  "Have a photo session with some friends",
+  "Learn calligraphy",
+  "Learn Express.js",
+  "Plan a trip to another country",
+  "Take a hike at a local park",
+  "Bake pastries for me and neighbor",
+  "Contribute code or a monetary donation to an open-source software project",
+  "Watch a classic movie",
+  "Do something nice for someone I care about",
+];
 
-const $usersList = document.getElementById("users-list");
+const $todoList = document.getElementById("todos");
 
-for (let user of users) {
-  $usersList.innerHTML += CreateUser(user);
-}
+for (let todo of todos) {
+  const $newTodo = document.createElement("li");
+  $newTodo.classList.add("todo");
+  $newTodo.innerText = todo;
+  $newTodo.addEventListener("click", function () {
+    this.classList.toggle("finished");
+  });
 
-function CreateUser(user) {
-  return `
-    <tr>
-      <td>${user.id}</td>
-      <td>${user.name}</td>
-      <td>${user.email}</td>
-      <td>${user.address.city}</td>
-      <td>${user.phone}</td>
-      <td>${user.company.name}</td>
-      <td><a href="details.html?id=${user.id}">Voir</a></td>
-    </tr>
-  `;
+  const $deleteBtn = document.createElement("button");
+  $deleteBtn.innerText = "x";
+  $deleteBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    this.parentElement.remove();
+  });
+
+  $newTodo.append($deleteBtn);
+  $todoList.append($newTodo);
 }
